6d7efbb883fa39ef025110046107cfe7c658bab5
vovapasko <vovapasko1699@gmail.com>
2024-10-30 17:28:41 +0100

    implemented anagram history search and made tests for that

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,7 +10,19 @@ fun main() {
 }
 
 fun areAnagrams(str1: String, str2: String): Boolean {
-    return str1.lowercase().filter { it.isLetter() }.groupBy { it }.mapValues { it.value.size } ==
-            str2.lowercase().filter { it.isLetter() }.groupBy { it }.mapValues { it.value.size }
+    return str1.transformToMap() ==
+            str2.transformToMap()
 }
+
+fun String.transformToMap(): Map<Char, Int> {
+    return this.lowercase().filter { it.isLetter() }.groupBy { it }.mapValues { it.value.size }
+}
+
+
+// Returns all string anagrams from input history
+fun anagramFromHistory(history: Map<String, Map<Char, Int>>, inputString: String): List<String> {
+    val anagrams = history.filterValues { value -> value == inputString.transformToMap() }
+    return anagrams.keys.toList()
+}
+
 
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,8 +1,12 @@
+import org.example.anagramFromHistory
 import org.example.areAnagrams
+import org.example.transformToMap
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class MainKtTest {
+
 
     @Test
     fun testWordsAreAnagrams() {
@@ -14,6 +18,36 @@ class MainKtTest {
     fun testTextsAreAnagrams() {
         assertTrue(areAnagrams("New York Times", "monkeys write"))
         assertTrue(areAnagrams("Church of Scientology", "rich-chosen goofy cult"))
+        assertTrue(areAnagrams("William Shakespeare", "I am a weakish speller"))
+        assertTrue(areAnagrams("William Shakespeare", "I'll make a wise phrase"))
+    }
+
+
+    @Nested
+    inner class TestAnagramsFromHistory {
+        @Test
+        fun testAnagramsInHistory() {
+            val str1 = "New York Times"
+            val str2 = "Madam Curie"
+            val str3 = "George Bush"
+            val str4 = "I am a weakish speller"
+            val str5 = "William Shakespeare"
+            val str6 = "williaMshakespEare"
+
+            val history = mapOf(
+                str1 to str1.transformToMap(),
+                str2 to str2.transformToMap(),
+                str3 to str3.transformToMap(),
+                str4 to str4.transformToMap(),
+                str5 to str5.transformToMap(),
+                str6 to str6.transformToMap()
+            )
+
+            val input = "I'll make a wise phrase"
+            val output = listOf(str4, str5, str6)
+
+            assertEquals(output, anagramFromHistory(history, input))
+        }
     }
 
 }
